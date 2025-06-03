1578d2e56daffa23622c2ecb3e76c4734b16855c
amishasao <mintysao@gmail.com>
2025-06-03 18:17:18 -0500

    using type checks

--- my_abs.py
+++ my_abs.py
@@ -1,5 +1,6 @@
 import math
 
+
 def my_abs(x):
     try:
         if x < 0:
@@ -8,3 +9,7 @@ def my_abs(x):
             return x
     except TypeError:
         return math.nan
+
+
+def my_almost_eq(x, y):
+    return my_abs(x - y) < 1e-16
